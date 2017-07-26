2090dbddc03ffd99d9aa4b77a854e8aecfdac010
Olly.W <yonaichin@gmail.com>
2017-07-26 17:03:29 +0800

    add sample page

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
-
 import ejs from 'ejs'
+
+import _CONFIG from '../config.json'
 
 const _PORT = process.env.PORT || 3000
 let app = express()
@@ -8,10 +9,11 @@ let app = express()
 app.set('views', `${__dirname}/../views`)
 app.set('view engine', 'ejs')
 
-app.use(express.static(__dirname + '/public'))
+// app.use('/static', express.static(__dirname + 'public'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', _CONFIG)
 })
 
 app.listen(_PORT, () => {
