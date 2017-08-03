1424ea3439c76fab5784e5150400f145ba345ebc
Olly.W <yonaichin@gmail.com>
2017-08-03 17:05:04 +0800

    add contact us

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,11 +10,23 @@ var isMobile = !!md.mobile();
     if (isMobile) {
       setTimeout(function() {
         $('.tap-target').tapTarget('open');
-      }, 1500);
+      }, 500);
       setTimeout(function() {
         $('.tap-target').tapTarget('close');
       }, 5000);
+
     }
+    $('.map').tinyMap({
+      'center': '台中市烏日區太明路202號',
+      'zoom'  : 14,
+      'marker': [
+        {
+          'id': '雄利水泥',
+          'addr': '台中市烏日區太明路202號',
+          'text': '雄利水泥',
+        }
+      ]
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
