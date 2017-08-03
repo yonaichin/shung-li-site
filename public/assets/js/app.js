553fe8bc134ac5fc903f6d67059fe920bb4e9f1a
Olly.W <yonaichin@gmail.com>
2017-08-03 18:16:07 +0800

    use google map

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,6 @@
 var md = new MobileDetect(window.navigator.userAgent);
 var isMobile = !!md.mobile();
+var map
 
 (function($){
   $(function(){
@@ -16,17 +17,30 @@ var isMobile = !!md.mobile();
       }, 5000);
 
     }
-    $('.map').tinyMap({
-      'center': '台中市烏日區太明路202號',
-      'zoom'  : 14,
-      'marker': [
-        {
-          'id': '雄利水泥',
-          'addr': '台中市烏日區太明路202號',
-          'text': '雄利水泥',
-        }
-      ]
-    });
+    // $('.map').tinyMap({
+    //   'center': '台中市烏日區太明路202號',
+    //   'zoom'  : 14,
+    //   'marker': [
+    //     {
+    //       'id': '雄利水泥',
+    //       'addr': '台中市烏日區太明路202號',
+    //       'text': '雄利水泥',
+    //     }
+    //   ]
+    // });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function initMap() {
+  var addr = {lat: 24.0606989, lng: 120.6486733}
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: addr,
+    zoom: 14
+  });
+  var marker = new google.maps.Marker({
+    position: addr,
+    map: map,
+    title: '雄利水泥'
+  });
+}
