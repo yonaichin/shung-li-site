e1100a56e966c9759fe6fda8eca7650fa5addf50
Olly.W <yonaichin@gmail.com>
2017-08-03 15:38:23 +0800

    add mobile-menu future discovery effect

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,20 @@
+var md = new MobileDetect(window.navigator.userAgent);
+var isMobile = !!md.mobile();
+
 (function($){
   $(function(){
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+
+    if (isMobile) {
+      setTimeout(function() {
+        $('.tap-target').tapTarget('open');
+      }, 1500);
+      setTimeout(function() {
+        $('.tap-target').tapTarget('close');
+      }, 5000);
+    }
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
