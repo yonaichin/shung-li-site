e14a4a14cfbfeb904a1b93e3fbba54659ac72362
Olly.W <yonaichin@gmail.com>
2017-08-03 15:18:59 +0800

    use material design

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 import express from 'express'
 import ejs from 'ejs'
+import _ from 'lodash'
 
 import _CONFIG from '../config.json'
 
 const _PORT = process.env.PORT || 3000
+const env = process.env.NODE_ENV || 'development'
 let app = express()
 
 app.set('views', `${__dirname}/../views`)
 app.set('view engine', 'ejs')
 
-// app.use('/static', express.static(__dirname + 'public'))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', _CONFIG)
+  res.render('index', _.assign({}, _CONFIG, { env }))
 })
 
 app.listen(_PORT, () => {
